570c49efab3feffc9b2517f0d5cddccba4ade022
Soren I. Bjornstad <soren.bjornstad@gmail.com>
2014-07-02 11:10:02 -0500

    add test to be sure rtf output format hasn't changed

--- tests/test_rtfexport.py
+++ tests/test_rtfexport.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import filecmp
 sys.path.append("../")
 
 from db.questions import Question
@@ -8,6 +9,7 @@ from db.rtfexport import *
 class RtfExportTests(unittest.TestCase):
     def testRender(self):
         fname = 'testfile.rtf'
+        against_fname = 'resources/test_format_against.rtf'
         questions = [Question("Hello?", ["foo", "bar", "baz", "quux"], "c")]
         render(questions, fname)
         try:
@@ -16,6 +18,8 @@ class RtfExportTests(unittest.TestCase):
             self.assertTrue(False), "File was not created successfully"
         else:
             self.assertTrue(f.readlines()), "No data in file"
+            assert filecmp.cmp(fname, against_fname), \
+                    "Output different from saved correct output file"
         finally:
             f.close()
             import os
