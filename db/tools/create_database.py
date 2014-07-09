bda2802dd8f227004fe3703df3bc1107d0ecdf29
Soren I. Bjornstad <soren.bjornstad@gmail.com>
2014-07-09 16:04:26 -0500

    change history/classes schema
    This is not much of a change since we haven't used these tables anywhere
    yet.

--- db/tools/create_database.py
+++ db/tools/create_database.py
@@ -5,9 +5,9 @@ def makeDatabase(fname):
     connection = sqlite.connect(fname)
     cursor = connection.cursor()
     cursor.execute('CREATE TABLE questions (qid INTEGER PRIMARY KEY, ord INTEGER, q TEXT, ca TEXT, answers TEXT, sid INTEGER)')
-    cursor.execute('CREATE TABLE classes (cid INTEGER PRIMARY KEY, name TEXT, setsReview TEXT, setsNew TEXT)')
+    cursor.execute('CREATE TABLE classes (cid INTEGER PRIMARY KEY, name TEXT)')
     cursor.execute('CREATE TABLE sets (sid INTEGER PRIMARY KEY, name TEXT, num INTEGER)')
-    cursor.execute('CREATE TABLE history (hid INTEGER PRIMARY KEY, sid INTEGER, introOrd INTEGER)')
+    cursor.execute('CREATE TABLE history (hid INTEGER PRIMARY KEY, cid INTEGER, sid INTEGER, introOrd INTEGER)')
     cursor.execute('CREATE TABLE conf (conf TEXT)')
     return connection
 
