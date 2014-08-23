e7af6816f07be397f5f4315a4699b0637757a687
Soren I. Bjornstad <soren.bjornstad@gmail.com>
2014-08-23 14:57:24 -0500

    update database schema
    Some new columns are needed for the scheduling system.

--- db/tools/create_database.py
+++ db/tools/create_database.py
@@ -5,9 +5,9 @@ def makeDatabase(fname):
     connection = sqlite.connect(fname)
     cursor = connection.cursor()
     cursor.execute('CREATE TABLE questions (qid INTEGER PRIMARY KEY, ord INTEGER, q TEXT, ca TEXT, answers TEXT, sid INTEGER)')
-    cursor.execute('CREATE TABLE classes (cid INTEGER PRIMARY KEY, name TEXT)')
+    cursor.execute('CREATE TABLE classes (cid INTEGER PRIMARY KEY, name TEXT, setsUsed INTEGER)')
     cursor.execute('CREATE TABLE sets (sid INTEGER PRIMARY KEY, name TEXT, num INTEGER)')
-    cursor.execute('CREATE TABLE history (hid INTEGER PRIMARY KEY, cid INTEGER, sid INTEGER, introOrd INTEGER)')
+    cursor.execute('CREATE TABLE history (hid INTEGER PRIMARY KEY, cid INTEGER, qid INTEGER, lastSet INTEGER, factor INTEGER, priority INTEGER)')
     cursor.execute('CREATE TABLE conf (conf TEXT)')
     return connection
 
