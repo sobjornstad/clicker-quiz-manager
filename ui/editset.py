e138932c439748627e616cfaee03e8e223b72b06
Soren I. Bjornstad <soren.bjornstad@gmail.com>
2015-06-27 12:59:44 -0500

    remove pointless use of lambda function
    I probably understood a bit less about the way passing a function as an
    argument worked when I wrote that, I guess.  That or I wasn't paying
    attention to what I was writing -- maybe equally likely!

--- ui/editset.py
+++ ui/editset.py
@@ -51,7 +51,7 @@ class SetEditor(QDialog):
         self.form.jumpCombo.activated.connect(self.onJumpToSet)
         self.form.questionList.itemSelectionChanged.connect(self.onQuestionChange)
         qlShortcut = QShortcut(QKeySequence("Alt+L"), self.form.questionList)
-        qlShortcut.connect(qlShortcut, QtCore.SIGNAL("activated()"), lambda: self.form.questionList.setFocus())
+        qlShortcut.connect(qlShortcut, QtCore.SIGNAL("activated()"), self.form.questionList.setFocus)
         saveShortcut = QShortcut(QKeySequence("Ctrl+S"), self.form.saveButton)
         saveShortcut.connect(saveShortcut, QtCore.SIGNAL("activated()"), self.onSaveQuestion)
         self.form.questionList.model().rowsMoved.connect(self.onDragDrop)
