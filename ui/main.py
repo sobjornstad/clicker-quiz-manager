388bd38059fa9112c9fcd11e926ffad733331af6
Soren I. Bjornstad <soren.bjornstad@gmail.com>
2015-09-02 15:01:32 -0500

    bump version

--- ui/main.py
+++ ui/main.py
@@ -23,7 +23,7 @@ import ui.history
 import ui.prefs
 import ui.utils as utils
 
-APPLICATION_VERSION = "1.0.4"
+APPLICATION_VERSION = "1.1.0"
 
 class MainWindow(QMainWindow):
     def __init__(self):
